410f0c61be2d59640c8ab80fe56701147ba4206c
Stefano Garzarella <sgarzare@redhat.com>
2023-11-22 16:36:20 +0100

    examples/svsm: cleanups
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>

--- examples/svsm.rs
+++ examples/svsm.rs
@@ -25,8 +25,9 @@ impl Write for UnixConnection {
         self.0.write_all(buf).map_err(|_| CPError::WriteError)?;
         Ok(buf.len())
     }
+
     fn flush(&mut self) -> Result<(), CPError> {
-        Ok(self.0.flush().map_err(|_| CPError::FlushError)?)
+        self.0.flush().map_err(|_| CPError::FlushError)
     }
 }
 
@@ -114,13 +115,13 @@ fn main() {
 
     let workload_id = "snp-workload".to_string();
 
-    let url = env::args().nth(1).unwrap_or("http://127.0.0.1:8000".into());
+    let url_server = env::args().nth(1).unwrap_or("http://127.0.0.1:8000".into());
     let client = reqwest::blocking::ClientBuilder::new()
         .cookie_store(true)
         .build()
         .unwrap();
 
-    info!("Connecting to KBS at {url}");
+    info!("Connecting to KBS at {url_server}");
 
     let mut attestation = AttestationReport::default();
     attestation.measurement[0] = 42;
@@ -132,7 +133,7 @@ fn main() {
         .unwrap();
 
     let resp = client
-        .post(url.clone() + "/kbs/v0/register_workload")
+        .post(url_server.clone() + "/kbs/v0/register_workload")
         .json(&registration)
         .send()
         .unwrap();
@@ -161,8 +162,12 @@ fn main() {
         };
         let req: Request = serde_json::from_value(data).unwrap();
 
-        let http_req = client.post(url.clone() + &req.endpoint).json(&req.body);
-        debug!("HTPP request - {:#?}", http_req);
+        let url = url_server.clone() + &req.endpoint;
+        let http_req = match req.method {
+            HttpMethod::GET => client.get(url).json(&req.body),
+            HttpMethod::POST => client.post(url).json(&req.body),
+        };
+        debug!("HTTP request - {:#?}", http_req);
 
         let http_resp = http_req.send().unwrap();
         debug!("HTTP response - {:#?}", http_resp);
@@ -171,7 +176,9 @@ fn main() {
             status: http_resp.status().as_u16(),
             body: http_resp.text().unwrap_or(String::new()),
         };
-        proxy.write_json(&json!(resp)).unwrap();
+        if proxy.write_json(&json!(resp)).is_err() {
+            break;
+        }
     }
 
     svsm.join().unwrap();
