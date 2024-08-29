5c8139d7e6a0a6ea6fafedef9d43ebb615887bde
Tyler Fanelli <tfanelli@redhat.com>
2024-08-28 21:06:47 -0400

    raclients: Add optional frontend traits
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

---
+++ raclients/src/frontend/proxy.rs
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use alloc::{fmt::{self, Display, Formatter}, string::String};
+
+#[derive(Debug)]
+pub enum Error {
+    Eof,
+    UnexpectedEof,
+    WriteZero,
+    Other(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Eof => write!(f, "End of file encountered"),
+            Self::UnexpectedEof => write!(f, "Unexpected end of file encountered"),
+            Self::WriteZero => write!(f, "Zero bytes written"),
+            Self::Other(s) => write!(f, "{}", &s),
+        }
+    }
+}
+
+pub trait RacProxyRead {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Error>;
+
+    fn read_exact(&mut self, mut buf: &mut [u8]) -> Result<(), Error> {
+        let mut read = 0;
+        while !buf.is_empty() {
+            match self.read(buf) {
+                Ok(0) => break,
+                Ok(n) => {
+                    let tmp = buf;
+                    buf = &mut tmp[n..];
+                    read += n;
+                }
+                Err(e) => return Err(e),
+            }
+        }
+        if !buf.is_empty() {
+            if read == 0 {
+                Err(Error::Eof)
+            } else {
+                Err(Error::UnexpectedEof)
+            }
+        } else {
+            Ok(())
+        }
+    }
+}
+
+pub trait RacProxyWrite {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+    fn flush(&mut self) -> Result<(), Error>;
+
+    fn write_all(&mut self, mut buf: &[u8]) -> Result<(), Error> {
+        while !buf.is_empty() {
+            match self.write(buf) {
+                Ok(0) => {
+                    return Err(Error::WriteZero);
+                }
+                Ok(n) => buf = &buf[n..],
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(())
+    }
+}
+
+pub trait RacProxyConnection: RacProxyRead + RacProxyWrite {}
--- raclients/src/lib.rs
+++ raclients/src/lib.rs
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #![no_std]
+
+extern crate alloc;
+
+#[cfg(feature = "frontend")]
+pub mod frontend;
